cc262f168dfb091d3d524ae1406036f253f78a5e
K1nlaS <stainedlake@gmail.com>
2022-10-01 16:48:30 +0300

    adding initial image loading functionality

--- src/App.js
+++ src/App.js
@@ -13,10 +13,53 @@ const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 
 function App() {
+
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+
+  const fetchImages = async () => {
+    setLoading(true);
+    let url = `${mainUrl}${clientID}`;
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setPhotos(data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("handle submit");
+  };
+
   return (
-    <div>
-      <h2>Stock Photos Search</h2>
-    </div>
+    <main>
+      <section className="search">
+        <form className="search-form">
+          <input type="text" placeholder="Search Photos" className="form-input" />
+          <button type="submit" className="submit-btn" onClick={handleSubmit}><FaSearch /></button>
+        </form>
+      </section>
+
+      <section className="photos">
+        <div className="photos-center">
+          {
+            photos.map(photo => <Photo key={photo.id} {...photo} />)
+          }
+
+          {loading && <h2 className="loading">Loading...</h2>}
+        </div>
+      </section>
+    </main>
   );
 }
 
